ba8045f9501c0b028c0d3068491aae7bf06fb8fe
LatvianModder <latvianmodder@gmail.com>
2017-04-26 23:23:40 +0300

    Minor gui changes, renamed *Info* to *Guide*, moved guide API to FTBLib, improved list line, added partial FTBUtilities Ranks Guide

--- src/main/java/com/latmod/yabba/block/BlockAntibarrel.java
+++ src/main/java/com/latmod/yabba/block/BlockAntibarrel.java
@@ -4,14 +4,17 @@ import com.latmod.yabba.tile.TileAntibarrel;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import net.minecraftforge.common.util.Constants;
 
 import javax.annotation.Nullable;
+import java.util.List;
 
 /**
  * Created by LatvianModder on 15.12.2016.
@@ -43,6 +46,20 @@ public class BlockAntibarrel extends BlockBarrelBase
     @Override
     public void placeFromItem(ItemStack stack, @Nullable TileEntity tile)
     {
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, EntityPlayer player, List<String> tooltip, boolean advanced)
+    {
+        if(stack.hasTagCompound() && stack.getTagCompound().hasKey("BlockEntityTag"))
+        {
+            NBTTagList list = stack.getTagCompound().getCompoundTag("BlockEntityTag").getTagList("Inv", Constants.NBT.TAG_COMPOUND);
+
+            if(list.tagCount() > 0)
+            {
+                tooltip.add(list.tagCount() + " items");
+            }
+        }
     }
 
     @Override
