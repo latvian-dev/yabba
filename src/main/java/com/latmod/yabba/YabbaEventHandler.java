c4d784d35649a8ad1def686048ed77c379254586
LatvianModder <latvianmodder@gmail.com>
2017-09-28 03:47:14 +0300

    Removed client side ReloadEvent, renamed to ServerReloadEvent, added sidebar button groups

--- src/main/java/com/latmod/yabba/YabbaEventHandler.java
+++ src/main/java/com/latmod/yabba/YabbaEventHandler.java
@@ -1,7 +1,7 @@
 package com.latmod.yabba;
 
 import com.feed_the_beast.ftbl.api.EventHandler;
-import com.feed_the_beast.ftbl.api.ReloadEvent;
+import com.feed_the_beast.ftbl.api.ServerReloadEvent;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -14,20 +14,17 @@ public class YabbaEventHandler
 	public static final ResourceLocation RELOAD_CONFIG = new ResourceLocation(Yabba.MOD_ID, "config");
 
 	@SubscribeEvent
-	public static void registerReloadIds(ReloadEvent.RegisterIds event)
+	public static void registerReloadIds(ServerReloadEvent.RegisterIds event)
 	{
 		event.register(RELOAD_CONFIG);
 	}
 
 	@SubscribeEvent
-	public static void onReload(ReloadEvent event)
+	public static void onServerReload(ServerReloadEvent event)
 	{
-		if (event.getSide().isServer())
+		if (event.reload(RELOAD_CONFIG))
 		{
-			if (event.reload(RELOAD_CONFIG))
-			{
-				YabbaConfig.sync();
-			}
+			YabbaConfig.sync();
 		}
 	}
 }
